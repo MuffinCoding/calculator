eb0d9dcb7bb4e61baf4ad4b089ffa5172b106ff8
asaf <asafger134@gmail.com>
2023-09-10 13:28:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -41,11 +41,11 @@ def associative(op):
 
 def precedence(op):
     match op:
-        case "^":
+        case "^" | ":POW":
             return 4
-        case "*" | "/" | "%":
+        case "*" | "/" | "%" | ":MULTI" | ":DIV" | ":MOD":
             return 3
-        case "-" | "+":
+        case "-" | "+" | ":SUB" | ":SUM":
             return 2
     return 1
 
@@ -59,14 +59,11 @@ def rpn(tokens: list[str]) -> list[str]:
         
         if is_number:
             output.append(token)
-        if is_string:
-            if token.startswith(":"):
-                operators.append(token)
-                continue
+        if is_string and (not token.startswith(":") or token in [":MEM", ":DEL"]):
             output.append(token)
 
         match token:
-            case "^" | "*" | "/" | "%" | "-" | "+":
+            case "^" | "*" | "/" | "%" | "-" | "+" | ":POW" | ":MULTI" | ":DIV" | ":MOD" | ":SUB" | ":SUM":
                 if len(operators) > 0 and operators[-1] != "("and (precedence(token) < precedence(operators[-1]) or (precedence(token) == precedence(operators[-1]) and associative(token) == "L")):
                     output.append(operators.pop())
                 operators.append(token)
@@ -102,7 +99,32 @@ def operate(left: int | float, right: int | float, op: str) -> int | float:
         case "+":
             return left + right
 
-def calculate(string: str, vars: dict) -> float | int:
+def parse(token: str | float | int, mem: dict) -> float | int:
+    if type(token) != int and type(token) != float and token not in mem:
+        return float(token) if "." in list(token) else int(token)
+    elif token in mem:
+        return mem[token]
+    else:
+        return token
+
+def parse_command(token: str) -> str:
+    match token:
+        case ":SUM":
+            return "+"
+        case ":SUB":
+            return "-"
+        case ":MULTI":
+            return "*"
+        case ":DIV":
+            return "/"
+        case ":MOD":
+            return "%"
+        case ":POW":
+            return "^"
+        case _:
+            return "+"
+
+def calculate(string: str, mem: dict) -> float | int:
     tokens = tokenizer(string)
 
     tokens = rpn(tokens)
@@ -110,38 +132,27 @@ def calculate(string: str, vars: dict) -> float | int:
     x = 0
 
     while True:
-        print(tokens, x, tokens[x])
+        # print(tokens, x)
 
         if len(tokens) == 1:
             is_string = any(x in [":", "_", "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"] for x in list(tokens[0]))
 
             if is_string:
-                if tokens[0] in vars:
-                    new = vars[tokens[0]] if type(vars[tokens[0]]) != str else float(vars[tokens[0]]) if "." in list(vars[tokens[0]]) else int(vars[tokens[0]])
+                if tokens[0] in mem:
+                    new = mem[tokens[0]] if type(mem[tokens[0]]) != str else float(mem[tokens[0]]) if "." in list(mem[tokens[0]]) else int(mem[tokens[0]])
                     tokens.pop()
                     tokens.append(new)
+
+        if tokens == []:
+            tokens = [0]
 
         match tokens[x]:
             case "^" | "*" | "/" | "%" | "-" | "+":
                 op = tokens.pop(x)
 
-                right = math.pi
-                if type(tokens[x - 1]) != int and type(tokens[x - 1]) != float and tokens[x - 1] not in vars:
-                    right = float(tokens.pop(x - 1)) if "." in list(tokens[x - 1]) else int(tokens.pop(x - 1))
-                elif tokens[x - 1] in vars:
-                    right = float(vars[tokens[x - 1]]) if "." in list(tokens[x - 1]) else int(vars[tokens[x - 1]])
-                    tokens.pop(x - 1)
-                else:
-                    right = tokens.pop(x - 1)
+                right = parse(tokens.pop(x - 1), mem)
 
-                left = math.pi
-                if type(tokens[x - 2]) != int and type(tokens[x - 2]) != float and tokens[x - 2] not in vars:
-                    left = float(tokens.pop(x - 2)) if "." in list(tokens[x - 2]) else int(tokens.pop(x - 2))
-                elif tokens[x - 2] in vars:
-                    left = float(vars[tokens[x - 2]]) if "." in list(tokens[x - 2]) else int(vars[tokens[x - 2]])
-                    tokens.pop(x - 2)
-                else:
-                    left = tokens.pop(x - 2)
+                left = parse(tokens.pop(x - 2), mem)
 
                 tokens.insert(x - 2, operate(left, right, op))
                 
@@ -149,46 +160,32 @@ def calculate(string: str, vars: dict) -> float | int:
             case "=":
                 tokens.pop(x)
 
-                right = math.pi
-                if type(tokens[x - 1]) != int and type(tokens[x - 1]) != float and tokens[x - 1] not in vars:
-                    right = float(tokens.pop(x - 1)) if "." in list(tokens[x - 1]) else int(tokens.pop(x - 1))
-                elif tokens[x - 1] in vars:
-                    right = float(vars[tokens[x - 1]]) if "." in list(tokens[x - 1]) else int(vars[tokens[x - 1]])
-                    tokens.pop(x - 1)
-                else:
-                    right = tokens.pop(x - 1)
+                right = parse(tokens.pop(x - 1), mem)
+
                 left = tokens.pop(x - 2)
 
-                vars[left] = right
+                mem[left] = right
 
-                tokens.append(vars[left])
+                tokens.append(mem[left])
 
                 x = 0
             case ":MEM":
                 tokens.pop()
-                tokens.append(vars)
-            case ":SUM":
-                tokens.pop(x)
+                tokens.append(mem)
+            case ":DEL":
+                mem.pop(tokens.pop(), None)
+                tokens.pop()
+            case _:
+                if type(tokens[x]) == str and tokens[x].startswith(":"):
+                    token = tokens.pop(x)
 
-                right = math.pi
-                if type(tokens[x - 1]) != int and type(tokens[x - 1]) != float and tokens[x - 1] not in vars:
-                    right = float(tokens.pop(x - 1)) if "." in list(tokens[x - 1]) else int(tokens.pop(x - 1))
-                elif tokens[x - 1] in vars:
-                    right = float(vars[tokens[x - 1]]) if "." in list(tokens[x - 1]) else int(vars[tokens[x - 1]])
-                    tokens.pop(x - 1)
-                else:
-                    right = tokens.pop(x - 1)
+                    right = parse(tokens.pop(x - 1), mem)
 
-                left = math.pi
-                if type(tokens[x - 2]) != int and type(tokens[x - 2]) != float and tokens[x - 2] not in vars:
-                    left = float(tokens.pop(x - 2)) if "." in list(tokens[x - 2]) else int(tokens.pop(x - 2))
-                elif tokens[x - 2] in vars:
-                    left = float(vars[tokens[x - 2]]) if "." in list(tokens[x - 2]) else int(vars[tokens[x - 2]])
-                    tokens.pop(x - 2)
-                else:
-                    left = tokens.pop(x - 2)
+                    left = parse(tokens.pop(x - 2), mem)
 
-                tokens.insert(x - 2, operate(left, right, "+"))
+                    tokens.insert(x - 2 if x - 2 >= 0 else x + 2, operate(left, right, parse_command(token)))
+
+                    x = 0
 
         x += 1
         if x > len(tokens) - 1:
@@ -200,17 +197,17 @@ def calculate(string: str, vars: dict) -> float | int:
     x = 0
 
     if len(tokens) == 1:
-        return tokens[0], vars
+        return tokens[0], mem
 
-    return calculate(calculate(" ".join(tokens[0]), vars))
+    return calculate(calculate(" ".join(tokens[0]), mem))
 
 def main():
-    vars = {}
+    mem = {}
 
     while True:
         inp = input("calc 2000 > ")
 
-        res, vars = calculate(inp, vars)
+        res, mem = calculate(inp, mem)
 
         print(res)
 
